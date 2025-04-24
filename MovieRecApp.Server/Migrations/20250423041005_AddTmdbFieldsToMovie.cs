c6a3c33952ba24dbf70a195f7bdc2202d332bca1
Arien Rassoul <ara269@uclive.ac.nz>
2025-04-24 13:54:47 +1200

    Scrapped TMDb enrichment and replaced with enrichment from Letterboxd film page and ajax http requests

---
+++ MovieRecApp.Server/Migrations/20250423041005_AddTmdbFieldsToMovie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRecApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTmdbFieldsToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TmdbId",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TmdbId",
+                table: "Movies");
+        }
+    }
+}
---
+++ MovieRecApp.Server/Migrations/20250424005352_AddYearAndRuntimeToMovies.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRecApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYearAndRuntimeToMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TmdbId",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Runtime",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Runtime",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TmdbId",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
